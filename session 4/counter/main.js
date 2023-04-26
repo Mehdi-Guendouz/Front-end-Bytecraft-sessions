11cae1d98d7cd60085691c64c91ae742df6c8a2c
Guendouz mehdi <mahdign12@gmail.com>
2023-04-26 16:38:40 +0100

    counter

--- session 4/counter/main.js
+++ session 4/counter/main.js
@@ -5,6 +5,7 @@ const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
+  console.log(btn);
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
     if (styles.contains("decrease")) {
